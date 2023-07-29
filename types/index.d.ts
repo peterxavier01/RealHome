88ac6251bf0a617f0edb10df418fc43230818f4c
Peter Uadiale <fxavy02@gmail.com>
2023-07-29 19:16:37 +0100

    update tsconfig.json file: fixed type declartio issue for react splide module

--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@splidejs/react-splide" {
-  export { Splide } from "../node_modules/@splidejs/react-splide/dist/types/components/Splide/Splide";
-  export { SplideSlide } from "../node_modules/@splidejs/react-splide/dist/types/components/SplideSlide/SplideSlide";
-  export { SplideTrack } from "../node_modules/@splidejs/react-splide/dist/types/components/SplideTrack/SplideTrack";
-}
