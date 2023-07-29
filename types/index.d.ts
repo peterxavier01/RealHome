f1ed07235f81a7a31f2519badf3763af6b7ae711
Peter Uadiale <fxavy02@gmail.com>
2023-07-29 01:58:01 +0100

    add types folder for types declaration

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+declare module "@splidejs/react-splide" {
+  export { Splide } from "../node_modules/@splidejs/react-splide/dist/types/components/Splide/Splide";
+  export { SplideSlide } from "../node_modules/@splidejs/react-splide/dist/types/components/SplideSlide/SplideSlide";
+  export { SplideTrack } from "../node_modules/@splidejs/react-splide/dist/types/components/SplideTrack/SplideTrack";
+}
