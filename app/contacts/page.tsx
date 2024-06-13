40ba0211d29e3c48865313a9612e64dce481e407
Peter Uadiale <fxavy02@gmail.com>
2024-06-13 22:47:31 +0100

    refactor: add map and contact form component

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import BreadCrumb from "@/components/BreadCrumb";
+import dynamic from "next/dynamic";
+import ContactForm from "@/components/ContactForm";
+
+const Map = dynamic(() => import("../../components/Map"), { ssr: false });
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="wrapper my-8">
+      <BreadCrumb routeOne={"home"} routeTwo={"About us"} href={"/"} />
+      <h1 className="font-light text-primary text-3xl md:text-[45px] leading-heading mt-2 mb-12">
+        Our <span className="font-medium">Contacts</span>
+      </h1>
 
-export default Contact
+      <section className="aspect-[3/1] bg-black/5">
+        <Map />
+      </section>
+
+      <ContactForm />
+    </div>
+  );
+};
+
+export default React.memo(Contact);
