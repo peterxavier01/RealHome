fd779e026f5c41d19e1fbf6e7b7c6f8de18024bb
Peter Uadiale <fxavy02@gmail.com>
2024-06-13 22:38:23 +0100

    refactor: modify fonts config

--- app/(home)/fonts.ts
+++ app/(home)/fonts.ts
@@ -2,8 +2,12 @@ import { Raleway, Playfair_Display } from "next/font/google";
 
 export const raleway = Raleway({
   subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-raleway",
 });
 
 export const playfair_display = Playfair_Display({
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-playfair",
 });
