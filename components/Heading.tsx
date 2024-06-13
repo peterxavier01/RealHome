87a2f174b31cb2eb8ff57cd42bf464285f1f3faa
Peter Uadiale <fxavy02@gmail.com>
2024-06-13 22:48:26 +0100

    refactor: make heading component more reusable

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -3,10 +3,12 @@
 import { twMerge } from "tailwind-merge";
 
 interface HeadingProps {
-  titleTextOne: string;
+  titleTextOne?: string;
   titleTextTwo?: string;
   className?: string;
   titleStyle?: string;
+  isSubHeader?: boolean;
+  subHeader?: string;
 }
 
 const Heading: React.FC<HeadingProps> = ({
@@ -14,12 +16,24 @@ const Heading: React.FC<HeadingProps> = ({
   titleTextTwo,
   className,
   titleStyle,
+  isSubHeader,
+  subHeader,
 }) => {
+  if (isSubHeader) {
+    return (
+      <h4 className="text-[37px] text-primary leading-[65px] font-medium font-raleway mb-4">
+        {subHeader}
+      </h4>
+    );
+  }
+
   return (
-    <div className={twMerge(`flex flex-col gap-6 mb-8`, className)}>
+    <div
+      className={twMerge(`flex flex-col gap-6 mb-8 font-raleway`, className)}
+    >
       <h2
         className={twMerge(
-          `text-[#3c3c3c] leading-[50px] text-3xl md:text-[45px]`,
+          `text-primary leading-heading text-3xl md:text-[45px]`,
           titleStyle
         )}
       >
@@ -28,7 +42,7 @@ const Heading: React.FC<HeadingProps> = ({
         </span>
         <span className="font-medium">{titleTextTwo}</span>
       </h2>
-      <span className="h-[3px] w-28 bg-[#e2574c] rounded-sm inline-block" />
+      <span className="h-[3px] w-28 bg-red-1000 rounded-sm inline-block" />
     </div>
   );
 };
