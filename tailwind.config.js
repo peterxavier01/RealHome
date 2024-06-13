e65a5656db468216f5d143414b8f807cea579a74
Peter Uadiale <fxavy02@gmail.com>
2024-06-13 22:36:50 +0100

    refactor: extend tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,10 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      "light-gray": "rgb(0 0 0 / 0.051)",
-      "dark-gray": "#303030",
-    }),
+    fontFamily: {
+      raleway: "var(--font-raleway)",
+      "playfair-display": "var(--font-playfair)",
+    },
     extend: {
       screens: {
         "max-sm": { max: "767px" },
@@ -18,6 +17,23 @@ module.exports = {
           min: "767px", // Minimum width
           max: "1023px", // Maximum width
         },
+      },
+      colors: {
+        "gray-1000": "#f3f2f2",
+        "gray-2000": "#242424",
+        "gray-3000": "#797979",
+        "gray-4000": "#e6e6e6",
+        primary: "#3c3c3c",
+        muted: "#c2c1c1",
+        "red-1000": "#e2574c",
+      },
+      backgroundColor: {
+        "light-gray": "rgb(0 0 0 / 0.051)",
+        "dark-gray": "#303030",
+      },
+      lineHeight: {
+        paragraph: "27px",
+        heading: "50px",
       },
     },
   },
