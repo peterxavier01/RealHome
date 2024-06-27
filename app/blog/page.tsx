f136a261146efd9ec0896483879810b70ba395bc
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 00:52:46 +0100

    refactor: modify blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,34 +1,31 @@
 import React from "react";
 
+import PageComponent from "./_components/PageComponent";
 import BreadCrumb from "@/components/BreadCrumb";
-import Paginate from "@/components/Paginate";
 
-import { posts } from "@/config/data";
-import Sidebar from "@/components/Sidebar";
-import PostCard from "./_components/PostCard";
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
 
-const Blog = () => {
+const BlogPage = ({ searchParams }: BlogPageProps) => {
+  const currentPage = Number(searchParams?.page ?? 1);
+
   return (
     <main className="blog wrapper">
       <section className="my-12 px-4">
-        <BreadCrumb href="/" routeOne="Home" routeTwo="Blog" />
+        <BreadCrumb
+          breadcrumbs={[
+            { name: "Home", href: "/" },
+            { name: "Blog", href: "#" },
+          ]}
+        />
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-7">
-          <div className="col-span-2 border-b-2 md:border-b-0 md:border-r-2 border-gray-1000 md:pr-10">
-            {posts.map((post) => (
-              <PostCard key={post.id} post={post} />
-            ))}
-
-            <div className="mb-12 md:mb-0">
-              <Paginate total={40} />
-            </div>
-          </div>
-
-          <Sidebar />
-        </div>
+        <PageComponent page={currentPage} />
       </section>
     </main>
   );
 };
 
-export default Blog;
+export default BlogPage;
