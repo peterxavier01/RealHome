b01d157e377c78df7687d4b2f5b5df742799e16f
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 00:44:12 +0100

    refactor: modify project types

--- types.ts
+++ types.ts
@@ -1,10 +1,75 @@
-export interface Card {
-  src: string;
-  title: string;
+interface Address {
   state: string;
   city: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  image?: string;
+  email: string;
+  role: string;
+  comments?: Comment[];
+  posts?: Post[];
+}
+
+export interface Comment {
+  id: string;
+  user: User;
+  content: string;
+  userId: string;
+  publishedAt: string;
+  post: Post;
+  postId: string;
+  replies: Comment[];
+  parentId?: string;
+  parent?: Comment;
+}
+
+export interface Author {
+  id: string;
+  name: string;
+  image?: string;
+  email: string;
+  role: string;
+}
+
+interface PostCategory {
+  categoryId: string;
+  id: string;
+  postId: string;
+}
+
+export interface Post {
+  id: string;
+  author: Author;
+  authorId: string;
+  comments: Comment[];
+  content: string;
+  description: string;
+  image: string;
+  lastUpdatedAt: string;
+  permalink: string;
+  postCategories: PostCategory[];
+  publishedAt: string;
+  title: string;
+}
+
+export interface Property {
+  id: string;
+  name: string;
+  image: string;
+  client: string;
+  info: string;
+  description: string;
+  category: string;
   price: string;
-  size: number;
+  location: Address;
+  relatedProperties: Property[];
+  inverseRelation?: Property;
+  inverseRelationId?: string;
+  lastUpdated: string;
+  size: string;
   numberOfBedrooms: number;
   numberOfBathrooms: number;
 }
