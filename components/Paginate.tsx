ccf72810c204a1e06f5f44ea3f304d73c72647f3
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 01:23:34 +0100

    refactor: modify custom pagination component

--- components/Paginate.tsx
+++ components/Paginate.tsx
@@ -1,28 +1,75 @@
 "use client";
 
 import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import Pagination from "rc-pagination";
+
 import Button from "./Button";
+
 import "../styles/index.scss";
 
-const itemRender = (
-  current: number,
-  type: "page" | "prev" | "next" | "jump-prev" | "jump-next",
-  element: React.ReactNode
-) => {
-  if (type === "prev") {
-    return <Button className="text-sm md:text-base">Previous</Button>;
-  }
-  if (type === "next") {
-    return <Button className="text-sm md:text-base">Next</Button>;
-  }
-  return element;
+const createItemRender = (createPageURL: (current: number) => void) => {
+  const ItemRender = (
+    current: number,
+    type: "page" | "prev" | "next" | "jump-prev" | "jump-next",
+    element: React.ReactNode
+  ) => {
+    if (type === "prev") {
+      return (
+        <Button
+          className="text-sm md:text-base"
+          onClick={() => createPageURL(current)}
+        >
+          Previous
+        </Button>
+      );
+    }
+
+    if (type === "next") {
+      return (
+        <Button
+          className="text-sm md:text-base"
+          onClick={() => createPageURL(current)}
+        >
+          Next
+        </Button>
+      );
+    }
+
+    if (type === "page") {
+      return (
+        <a rel="no-follow" onClick={() => createPageURL(current)}>
+          {current}
+        </a>
+      );
+    }
+    return element;
+  };
+
+  ItemRender.displayName = "ItemRender";
+
+  return ItemRender;
 };
 
-const Paginate = ({ total }: { total: number }) => (
-  <>
-    <Pagination total={total} itemRender={itemRender} />
-  </>
-);
+const Paginate = ({ total }: { total: number }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const createPageURL = (pageNumber: number | string) => {
+    const paramsObj = Array.from(searchParams).reduce(
+      (acc, [key, value]) => ({ ...acc, [key]: value }),
+      {}
+    );
+
+    const params = new URLSearchParams(paramsObj);
+    params.set("page", pageNumber.toString());
+    return router.push(`${pathname}?${params.toString()}`);
+  };
+
+  const itemRender = createItemRender(createPageURL);
+
+  return <Pagination total={total} itemRender={itemRender} />;
+};
 
 export default Paginate;
