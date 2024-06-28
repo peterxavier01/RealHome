cb95adc98913e254dff43c4e1af596ed7a9fb346
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 01:44:36 +0100

    refactor: modify logic to send message to admin

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,69 +1,77 @@
-import Link from "next/link";
-import Heading from "./Heading";
-import TextInput from "./TextInput";
-import Button from "./Button";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
 
-const contactDetails = [
-  { id: 1, name: "Tel:", value: "+180 603 6035", isLink: false },
-  { id: 2, name: "Fax:", value: "+1800 889 9898", isLink: false },
-  { id: 3, name: "Email:", value: "mail@demolink.org", isLink: true },
-];
+import Heading from "./Heading";
+import PostForm from "./PostForm";
+
+interface MutationArgs {
+  name: string;
+  email: string;
+  message: string;
+}
 
 const ContactForm = () => {
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValue({
+      ...value,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const resetForm = () => {
+    setValue({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  const mutation = useMutation({
+    mutationFn: (data: MutationArgs) => {
+      return axios.post("/api/message", data);
+    },
+
+    onSuccess: () => {
+      toast.success("Message Sent Successfully");
+      resetForm();
+    },
+
+    onError: () => {
+      toast.error("Something went wrong. Please try again");
+    },
+  });
+
+  const { isPending } = mutation;
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    mutation.mutate({
+      ...value,
+    });
+  }
+
   return (
-    <div className="mt-10 grid md:grid-cols-12 gap-6 md:gap-8">
-      <div className="md:col-span-5 lg:col-span-4">
-        <Heading isSubHeader subHeader="Contact Info" />
-        <p className="text-sm leading-6 text-gray-3000 font-playfair-display mb-3 max-w-full md:max-w-[343px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quidem,
-          accusantium laudantium eos temporibus sit iusto totam, tempora iste
-          fuga facilis incidunt recusandae, veniam dolore! Odio magni ab numquam
-          adipisci.
-        </p>
+    <div className="md:col-span-7 lg:col-span-8">
+      <Heading isSubHeader subHeader="Leave a Comment" />
 
-        <p className="text-gray-3000 leading-paragraph font-playfair-display font-bold mb-3 max-w-full md:max-w-[365px]">
-          Real Home INC.
-          <br />{" "}
-          <span className="uppercase">
-            st stephen&apos;s house, colston ave, bristol, bristol bs14st, uk
-          </span>
-        </p>
-
-        <div className="paragraph flex flex-col gap-1">
-          {contactDetails.map((item) => (
-            <p key={item.id}>
-              {item.name}{" "}
-              {item.isLink ? (
-                <Link href="#" className="text-red-1000 underline">
-                  {item.value}
-                </Link>
-              ) : (
-                <span className="font-medium text-gray-2000">{item.value}</span>
-              )}
-            </p>
-          ))}
-        </div>
-      </div>
-
-      <div className="md:col-span-7 lg:col-span-8">
-        <Heading isSubHeader subHeader="Leave a Comment" />
-
-        <form>
-          <div className="flex items-center gap-6 mb-6">
-            <TextInput placeholder="Your name" />
-            <TextInput placeholder="Your email" />
-          </div>
-
-          <TextInput placeholder="Your comment" isTextArea />
-
-          <div className="flex items-center gap-4 mb-6 mt-4">
-            <Button className="w-full max-w-[103px] h-[58px]">send</Button>
-            <Button className="bg-gray-1000 text-muted w-full max-w-[112px] h-[58px] hover:bg-gray-3000/90 hover:text-white">
-              clear
-            </Button>
-          </div>
-        </form>
-      </div>
+      <PostForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        resetForm={resetForm}
+        value={value}
+        isPending={isPending}
+      />
     </div>
   );
 };
