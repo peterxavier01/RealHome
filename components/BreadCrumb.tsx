90f6d411b5e658898731ec3aac9be0fc170a936e
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 01:17:59 +0100

    refactor: modify breadcrumb component

--- components/BreadCrumb.tsx
+++ components/BreadCrumb.tsx
@@ -1,41 +1,51 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-interface BreadCrumbProps {
-  routeOne: string;
-  routeTwo: string;
+interface Breadcrumb {
+  name: string;
   href: string;
 }
 
-const BreadCrumb: React.FC<BreadCrumbProps> = ({
-  routeOne,
-  routeTwo,
-  href,
-}) => {
+interface BreadCrumbProps {
+  breadcrumbs: Breadcrumb[];
+}
+
+const BreadCrumb: React.FC<BreadCrumbProps> = ({ breadcrumbs }) => {
   return (
-    <div className="flex items-center gap-3 font-raleway">
-      <Link
-        href={href}
-        className="text-red-1000 text-sm capitalize leading-heading"
-      >
-        {routeOne}
-      </Link>
-      <span>
-        <Image
-          src="images/arrow.svg"
-          alt="arrow-forward"
-          width={9}
-          height={5}
-          className="text-red-500"
-        />
-      </span>
-      <span className="text-primary text-sm leading-heading capitalize">
-        {routeTwo}
-      </span>
-    </div>
+    <ol className="flex items-center gap-3 font-raleway">
+      {breadcrumbs.map((breadcrumb, index) => {
+        const isLast = index === breadcrumbs.length - 1;
+
+        return (
+          <React.Fragment key={index}>
+            <li className="text-primary text-sm leading-heading capitalize">
+              {isLast ? (
+                <span className="line-clamp-1">{breadcrumb.name}</span>
+              ) : (
+                <Link
+                  href={breadcrumb.href}
+                  className="text-red-1000 text-sm capitalize leading-heading"
+                >
+                  {breadcrumb.name}
+                </Link>
+              )}
+            </li>
+            {!isLast ? (
+              <span>
+                <Image
+                  src="/images/arrow.svg"
+                  alt="arrow-forward"
+                  width={9}
+                  height={5}
+                  className="text-red-500"
+                />
+              </span>
+            ) : null}
+          </React.Fragment>
+        );
+      })}
+    </ol>
   );
 };
 
