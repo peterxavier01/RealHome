b7819a0730b586bc0605e7df2656a7c665a65160
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 01:08:26 +0100

    refactor: modify page breadcrumb

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,14 +3,19 @@
 import React from "react";
 import BreadCrumb from "@/components/BreadCrumb";
 import dynamic from "next/dynamic";
-import ContactForm from "@/components/ContactForm";
+import ContactFormSection from "@/components/ContactFormSection";
 
 const Map = dynamic(() => import("../../components/Map"), { ssr: false });
 
 const Contact = () => {
   return (
     <div className="wrapper my-8">
-      <BreadCrumb routeOne={"home"} routeTwo={"About us"} href={"/"} />
+      <BreadCrumb
+        breadcrumbs={[
+          { name: "Home", href: "/" },
+          { name: "Contact", href: "#" },
+        ]}
+      />
       <h1 className="font-light text-primary text-3xl md:text-[45px] leading-heading mt-2 mb-12">
         Our <span className="font-medium">Contacts</span>
       </h1>
@@ -19,7 +24,7 @@ const Contact = () => {
         <Map />
       </section>
 
-      <ContactForm />
+      <ContactFormSection />
     </div>
   );
 };
