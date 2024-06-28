b27ddd397a9d875b22f9466a86c6a1db52d549a1
Peter Uadiale <fxavy02@gmail.com>
2024-06-28 01:36:36 +0100

    feat: create toast provider

---
+++ providers/ToastProvider.tsx
@@ -0,0 +1,23 @@
+import { Flip, ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastProvider = () => {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={2500}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Flip}
+    />
+  );
+};
+
+export default ToastProvider;
